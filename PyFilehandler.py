1333f48b274d68bc3cecf3c41d54ec5d350e099b
DanielAndreasen <daniel.andreasen@hotmail.com>
2014-03-11 16:29:16 +0000

    Working version with clean example

--- PyFilehandler.py
+++ PyFilehandler.py
@@ -15,7 +15,8 @@ class Filehandler():
     Parameters
     ----------
     template : ASCII
-        A template for the datafile that should be created.
+        A template for the datafile that should be created. We just need the
+        PATH here.
     data : matrix
         Preferably a numpy array with all the data we want to copy in to the
         datafile with the same format as 'template'.
@@ -35,11 +36,12 @@ class Filehandler():
         ``numpy.loadtxt``.
     """
 
-    def __init__(self, template, data, output, skipheader=0, header=None,
+    def __init__(self, template, data, output, skipheader=None, header=None,
                  comments='#'):
         self.template = template
         self.data = data
         self.output = output
+        self.comments = comments
 
         # TODO - need to make this work better, so I'm able to read the ugly
         # data
@@ -48,31 +50,46 @@ class Filehandler():
                 self.data = np.array(self.data)
             except:  # Which error do we expect here
                 print "Not able to convert data to a numpy array!"
+
         self.skipheader = skipheader
+        try:
+            self.skipheader = int(abs(self.skipheader))
+        except TypeError:
+            print "Warning: The skipheader argument must be a positive integer"
+            print "         Setting the skipheader to 0 (no header added).\n"
+            self.skipheader = 0
+        
         self.header = header
+        if self.header == None:
+            print "The header argument was set to None. No header will be added."
+            self.header = ''
+        if self.header.count('\n') + 1 != self.skipheader:
+            print "Warning: The length of the header is different from the"
+            print "         skipheader paramter.\n"
 
     def create_header(self):
-        #TODO - Make the try/except to work!
-        try:
-            if not self.header:
-                with open(self.template, "r") as tmp:
-                    self.header = ''
-                    for i in xrange(self.skipheader):
-                        self.header += tmp.readline()
-
-                return self.header[0:-1]
-            else:
-                return self.header
-        except:
-            raise 'No header information given...'
+        if self.header == '':
+            with open(self.template, "r") as tmp:
+                self.header = ''
+                for i in xrange(self.skipheader):
+                    self.header += tmp.readline()
+            return self.header[0:-1]
+        else:
+            return self.header
 
     def fmt_from_template(self):
+        # Large part of this is from
+        # www.stackoverflow.com/questions/21865757/use-python-to-handle-and-create-input-files-for-external-software
         with open(self.template, "r") as tmp:
-            for i in xrange(self.skipheader):
-                z = tmp.readline()
-            del z
-            tmpl = tmp.readline()  # Don't want the '\n' in the end
-            if tmpl[-1::] == '\n':
+            # Getting 1 line with the data to extract the format.
+            if self.skipheader != 0:
+                for i in xrange(self.skipheader):
+                    z = tmp.readline()
+                del z
+                tmpl = tmp.readline()
+            else:
+                tmpl = [line.strip() for line in tmp][-1]
+            if tmpl[-1::] == '\n':  # Don't want the '\n' in the end
                 tmpl = tmpl[:-1]
 
         pat = r'( *-?\d+\.(\d+))'
@@ -89,21 +106,22 @@ class Filehandler():
 
     def create(self):
         fmt = self.fmt_from_template()
+        self.header = self.create_header()
         with open(self.output, 'w') as f:
-            np.savetxt(f, self.data, fmt, header=self.header, comments='')
+            np.savetxt(f, self.data, fmt, header=self.header,
+                    comments=self.comments)
             print "Data saved in", self.output
 
 
 def main():
     data = np.loadtxt("data.dat")
     template = "template.txt"
-    header = 'This is a header\nAnd a second line'
-    data_ugly = ''
-    with open('data_ugly.dat') as tmp:
-        for i in range(99):
-            data_ugly += tmp.readline()
+    header = 'This is a header\nAnd a second line\nAnd when everything is beautiful'
 
-    t = Filehandler(template, data, "result.dat", skipheader=3, header=header)
+    t = Filehandler(template, data, "result.dat", skipheader=3, header=header,
+            comments=' ')
+#    t = Filehandler(template, data, "result.dat", header=header)
+#    t = Filehandler(template, data, "result.dat")
     t.create()
 
 
